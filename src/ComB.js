9d90e8f30bd460076dbd9a6034f56362320db86b
Prakash Paswan <82628149+prakashptech@users.noreply.github.com>
2021-12-17 03:27:44 -0800

    added useContext

--- src/ComB.js
+++ src/ComB.js
@@ -1,7 +1,16 @@
+import { useContext } from "react";
+import { FirstName, lastName } from "./App";
 import ComC from "./ComC";
 export default function ComB() {
+  const fname = useContext(FirstName);
+  const lname = useContext(lastName);
   return (
     <div>
+      <p>useContext</p>
+      <h1>
+        {fname}
+        {lname}
+      </h1>
       <ComC />
     </div>
   );
